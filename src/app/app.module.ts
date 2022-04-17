8cd7ff0db34f5963bbbe3f20e8c17f157defa7d0
chello1975 <chello1975@example.com>
2022-04-17 11:15:14 -0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { TitleComponent } from './components/title/title.component';
+import { PageContentComponent } from './components/page-content/page-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    AsideComponent,
+    TitleComponent,
+    PageContentComponent
   ],
   imports: [
     BrowserModule,
